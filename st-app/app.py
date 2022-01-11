98eb2b49838c510b1dcc607d6a17961b666cf4a7
Michael J. Booth <mjboothaus@gmail.com>
2022-01-11 23:42:19 +0000

    Tweaks to get the ticker runing correctly - progress

--- st-app/app.py
+++ st-app/app.py
@@ -1,42 +1,53 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import pandas as pd
-import numpy as np
+from pathlib import Path
+#import pandas as pd
+#import numpy as np
+
+from dynaconf import settings
 
 st.header("LKT Market Watch")
+
+st.subheader("by Emmeline Booth")
+# st.write(settings.NAME)
+
+def load_ticker_template():
+  with open("ticker_template.html") as f:
+    contents = f.read()
+  return contents
+
+contents = load_ticker_template()
+
+CODE_1 = "ASX:ANZ"
+LABEL_1 = "ANZ Bank"
+CODE_2 = "ASX:CBA"
+LABEL_2 = "Commonwealth Bank"
+CODE_3 = "ASX:NAB"
+LABEL_3 = "National Aust Bank"
+CODE_4 = "ASX:WBC"
+LABEL_4 = "Westpac Bank"
+CODE_5 = "ASX:MQG"
+LABEL_5 = "Macquarie Bank"
+
+
+contents = contents.replace("[CODE_1]", CODE_1)
+contents = contents.replace("[LABEL_1]", LABEL_1)
+
+contents = contents.replace("[CODE_2]", CODE_2)
+contents = contents.replace("[LABEL_2]", LABEL_2)
+
+contents = contents.replace("[CODE_3]", CODE_3)
+contents = contents.replace("[LABEL_3]", LABEL_3)
+
+contents = contents.replace("[CODE_4]", CODE_4)
+contents = contents.replace("[LABEL_4]", LABEL_4)
+
+contents = contents.replace("[CODE_5]", CODE_5)
+contents = contents.replace("[LABEL_5]", LABEL_5)
+
+
+# st.write(contents)
+
+components.html(contents)
+
 st.markdown("by [DataBooth.com.au](https://wwww.databooth.com.au)")
-
-# https://www.tradingview.com/widget/ticker-tape/
-
-components.html("""
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-  {
-  "symbols": [
-    {
-      "proName": "ASX:CBA",
-      "title": "CBA"
-    },
-    {
-      "proName": "ASX:WBC",
-      "title": "Westpac"
-    },
-    {
-      "proName": "ASX:AMP",
-      "title": "AMP"
-    },
-    {
-      "proName": "ASX:IAG",
-      "title": "Insurance Australia Group"
-    }
-  ],
-  "showSymbolLogo": true,
-  "colorTheme": "light",
-  "isTransparent": false,
-  "displayMode": "adaptive",
-  "locale": "en"
-}
-  </script>
-</div>
-""")
